7a5eabaff9a217a9613068951b176c539a7ce923
Deepesh Kumar Raja <dkr.muz@gmail.com>
2019-05-02 14:47:09 +0530

    hiway_homepage is modified to implement the conftest

--- ui_test/POM_hiway_asg/pages/hiway_homepage.py
+++ ui_test/POM_hiway_asg/pages/hiway_homepage.py
@@ -1,11 +1,12 @@
 import time
 from POM_hiway_asg.pages.constant_var import TIMESHEET_TAB_BTN
+from POM_hiway_asg.test_scripts.base import BasePage
 
 
-class HiwayHomepageOptions():
+class HiwayHomepageOptions(BasePage):
 
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
 
     def click_timesheet_tab(self):
         time.sleep(5)  # using this sleep because the page needed the API call to complete, but the API is taking time
